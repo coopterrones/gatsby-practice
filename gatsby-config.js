c37bb9c833220a7fe4e0d4f1d51632e4c36f287c
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-17 16:16:42 -0600

    Gatsby Config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,9 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "YOUR PERSONAL BLOG TITLE",
+    author: "YOUR NAME HERE",
+  },
   plugins: ["gatsby-plugin-sass"],
 };
