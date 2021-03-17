22a106f6d26a4295f2c25cdf0d9b7cf8fc3d134c
Cooper Terrones <cooper.terrones@outlook.com>
2021-03-17 16:17:07 -0600

    Add GraphQL and sample posts

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
   return (
     <footer>
-      <p>Created by Cooper Terrones, ©2021</p>
+      <p>Created by {data.site.siteMetadata.author}, ©2021</p>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
---
+++ src/posts/react.md
@@ -0,0 +1,12 @@
+___
+title: "The Great Gatsby Test"
+date: "2021-03-17"
+___
+
+This is your second blog post!
+
+##How easy is that -- steps bellow 
+
+1. Create a new post using your favorite CMS 0R a simple MD file
+2. Upload to your site!
+3. It's Live! 
